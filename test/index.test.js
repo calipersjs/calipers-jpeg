28bf24948e41f31b661d1b12f179c220d4be2447
David Xu <david.xu@lob.com>
2022-08-20 20:12:41 -0700

    modernizesd

--- test/index.test.js
+++ test/index.test.js
@@ -1,59 +1,60 @@
 'use strict';
 
-var fs     = require('fs');
-var path   = require('path');
-var expect = require('chai').expect;
-var jpeg   = require('../lib/index');
+const fs     = require('fs');
+const path   = require('path');
+const expect = require('chai').expect;
+const jpeg   = require('../lib/index');
 
-describe('jpeg', function () {
+describe('jpeg', () => {
 
-  describe('detect', function () {
-    it('should return true for a JPEG', function () {
-      var jpegPath = path.resolve(__dirname, 'fixtures/jpeg/123x456.jpg');
-      var result = jpeg.detect(fs.readFileSync(jpegPath));
+  describe('detect', () => {
+
+    it('should return true for a JPEG', () => {
+      const jpegPath = path.resolve(__dirname, 'fixtures/jpeg/123x456.jpg');
+      const result = jpeg.detect(fs.readFileSync(jpegPath));
       expect(result).to.eql(true);
     });
 
-    it('should return false for a non-JPEG', function () {
-      var pngPath = path.resolve(__dirname, 'fixtures/png/123x456.png');
-      var result = jpeg.detect(fs.readFileSync(pngPath));
+    it('should return false for a non-JPEG', () => {
+      const pngPath = path.resolve(__dirname, 'fixtures/png/123x456.png');
+      const result = jpeg.detect(fs.readFileSync(pngPath));
       expect(result).to.eql(false);
     });
+
   });
 
-  describe('measure', function () {
+  describe('measure', () => {
 
-    var fixtures = path.resolve(__dirname, 'fixtures/jpeg');
-    var files = fs.readdirSync(fixtures);
+    const fixtures = path.resolve(__dirname, 'fixtures/jpeg');
+    const files = fs.readdirSync(fixtures);
 
-    files.forEach(function (file) {
+    files.forEach((file) => {
 
-      var fileSplit = file.split(/x|\./);
-      var width = parseInt(fileSplit[0]);
-      var height = parseInt(fileSplit[1]);
-      var expectedOutput = {
+      const fileSplit = file.split(/x|\./);
+      const width = parseInt(fileSplit[0]);
+      const height = parseInt(fileSplit[1]);
+      const expectedOutput = {
         type: 'jpeg',
-        pages: [{ width: width, height: height }]
+        pages: [{ width, height }]
       };
 
-      it('should return the correct dimensions for ' + file, function () {
-        var jpegPath = path.resolve(fixtures, file);
-        var fd = fs.openSync(jpegPath, 'r');
+      it(`should return the correct dimensions for ${  file}`, () => {
+        const jpegPath = path.resolve(fixtures, file);
+        const fd = fs.openSync(jpegPath, 'r');
         return jpeg.measure(jpegPath, fd)
-        .bind({})
-        .then(function (result) {
-          expect(result).to.eql(expectedOutput);
-        })
-        .finally(function () {
-          fs.closeSync(fd);
-        });
+          .then((result) => {
+            expect(result).to.eql(expectedOutput);
+          })
+          .finally(() => {
+            fs.closeSync(fd);
+          });
       });
 
     });
 
-    it('should error with a corrupt JPEG', function () {
-      var jpegPath = path.resolve(__dirname, 'fixtures/corrupt/corrupt.jpg');
-      var fd = fs.openSync(jpegPath, 'r');
+    it('should error with a corrupt JPEG', () => {
+      const jpegPath = path.resolve(__dirname, 'fixtures/corrupt/corrupt.jpg');
+      const fd = fs.openSync(jpegPath, 'r');
       return expect(jpeg.measure(jpegPath, fd)).to.be.rejectedWith(Error);
     });
 
